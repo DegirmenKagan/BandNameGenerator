93c8497e8353d670fa81b8b8770a0d3ef269acb3
Kagan Degirmen <kagandegirmen@gmail.com>
2022-12-18 17:43:55 +0300

    main > customizing theme of the app

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Band Name Generator',
-      home: RandomWords(),
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.deepOrangeAccent,
+        foregroundColor: Colors.black,
+      )),
+      home: const RandomWords(),
     );
   }
 }
